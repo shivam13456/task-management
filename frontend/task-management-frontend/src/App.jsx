f13a8d206d283f1168ef2226d278d093c58ab694
shivam13456 <shivamti2222@gmail.com>
2025-10-29 18:11:04 +0530

    fixes

--- frontend/task-management-frontend/src/App.jsx
+++ frontend/task-management-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Login from "./pages/Login";
 import TodoCreate from "./pages/AddTask";
 import Navbar from "./components/NavBar";
