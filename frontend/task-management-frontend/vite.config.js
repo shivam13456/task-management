6778c55f6a987b3fea2a87f8e47b9edbe823d624
shivam13456 <shivamti2222@gmail.com>
2025-10-29 17:42:24 +0530

    cgange in vite config

--- frontend/task-management-frontend/vite.config.js
+++ frontend/task-management-frontend/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// ✅ React Router ke liye history fallback enable kiya gaya
 export default defineConfig({
   plugins: [react()],
   base: "./",
   build: {
     outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true,
+  },
+  preview: {
+    historyApiFallback: true,
   },
 });
