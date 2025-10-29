300b7f89467c565188d8b2ddbe741818581708d9
shivam13456 <shivamti2222@gmail.com>
2025-10-29 18:09:21 +0530

    issue fixes

--- frontend/task-management-frontend/vite.config.js
+++ frontend/task-management-frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// ✅ React Router ke liye history fallback enable kiya gaya
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",
   build: {
     outDir: "dist",
   },
